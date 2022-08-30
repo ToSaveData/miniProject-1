db813ddeb313a0e7758e05fef92b2161cba2989a
errandt <errandt@gmail.com>
2022-08-30 11:05:05 +0900

    project

--- Product.h
+++ Product.h
@@ -1,1 +1,27 @@
 #pragma once
+#ifndef _PRODUCT_H_
+#include <iostream>
+using namespace std;
+
+class Product
+{
+public:
+	Product(int ID= 0000, string productName= "", int productPrice= 0, string productSort= ""): 
+		m_ID(ID), m_productName(productName), m_productPrice(productPrice), m_productSort(productSort){ }
+	void setID();
+	string getID(string&) const;
+	void setProductName();
+	string getProductName(string&) const; 
+	void setProductPrice();
+	string getProductPrice(int&) const;
+	void setProductSort();
+	string getProductSort(string&) const;
+
+private:
+	int m_ID;
+	string m_productName; //T1, T2 (CT 제품), N1 (포터블 X-ray), SNAP(구강 카메라), Scanner(스캐너)
+	int m_productPrice;
+	string m_productSort; //CT, X-ray, DentalCam, DentalScanner
+};
+
+#endif // !_PRODUCT_H_
